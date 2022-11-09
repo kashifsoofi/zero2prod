a7a4d9c8639100878b5ad7b13925a65a1991f757
Kashif Soofi <kashif.soofi+git@gmail.com>
2022-11-09 23:33:40 +0000

    Email with link

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -20,16 +20,36 @@ async fn subscribe_returns_a_200_for_valid_request() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
+    app.cleanup_subscriptinos("ursula_le_guin@gmail.com".into()).await
+}
 
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+#[tokio::test]
+async fn subscribe_persists_the_new_subscriber() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = r#"
+        { "name":"le guin", "email":"ursula_le_guin1@gmail.com" }
+    "#;
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+  
+    // Act
+    app.post_subscriptions(body.into()).await;
+  
+    // Assert
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions",)
         .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch saved subscription.");
-
-    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    app.cleanup_subscriptinos("ursula_le_guin1@gmail.com".into()).await;
+  
+    assert_eq!(saved.email, "ursula_le_guin1@gmail.com");
     assert_eq!(saved.name, "le guin");
-
-    app.cleanup_subscriptinos(saved.email).await
+    assert_eq!(saved.status, "pending_confirmation");
 }
 
 #[tokio::test]
@@ -94,7 +114,7 @@ async fn subscribe_sends_a_confirmation_email_for_valid_data() {
     // Arrange
     let app = spawn_app().await;
     let body = r#"
-        { "name":"le guin1", "email":"ursula_le_guin1@gmail.com" }
+        { "name":"le guin2", "email":"ursula_le_guin2@gmail.com" }
     "#;
 
     Mock::given(path("/send"))
@@ -109,6 +129,51 @@ async fn subscribe_sends_a_confirmation_email_for_valid_data() {
 
     // Assert
     // Mock asserts on drop
-    app.cleanup_subscriptinos("ursula_le_guin1@gmail.com".into())
-        .await
+    app.cleanup_subscriptinos("ursula_le_guin2@gmail.com".into())
+        .await;
+}
+
+#[tokio::test]
+async fn subscribe_sends_a_confirmation_email_with_a_link() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = r#"
+        { "name":"le guin3", "email":"ursula_le_guin3@gmail.com" }
+    "#;
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        // We are not setting an expectation here anymore
+        // The test is focused on another aspect of the app
+        // behaviour.
+        .mount(&app.email_server)
+        .await;
+
+    // Act
+    app.post_subscriptions(body.into()).await;
+
+    // Assert
+    app.cleanup_subscriptinos("ursula_le_guin3@gmail.com".into())
+        .await;
+
+    // Get the first intercepted request
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    // Parse the body as JSON, starting from raw bytes
+    let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+
+    // Extract the link from one of the request fields.
+    let get_link = |s: &str| {
+        let links: Vec<_> = linkify::LinkFinder::new()
+            .links(s)
+            .filter(|l| *l.kind() == linkify::LinkKind::Url)
+            .collect();
+        assert_eq!(links.len(), 1);
+        links[0].as_str().to_owned()
+    };
+
+    let html_link = get_link(&body["Html-part"].as_str().unwrap());
+    let text_link = get_link(&body["Text-part"].as_str().unwrap());
+    // The two links should be identical
+    assert_eq!(html_link, text_link);
 }
