7caec5d54d9c64bba957f30b92458a0b50e7a4e7
Kashif Soofi <kashif.soofi+git@gmail.com>
2022-11-13 20:25:53 +0000

    Pass error message as query string parameter

--- src/configuration.rs
+++ src/configuration.rs
@@ -22,6 +22,7 @@ pub struct ApplicationConfiguration {
     pub port: u16,
     pub host: String,
     pub base_url: String,
+    pub hmac_secret: Secret<String>
 }
 
 #[derive(Clone, serde::Deserialize)]
--- src/startup.rs
+++ src/startup.rs
@@ -9,6 +9,7 @@ use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
+use secrecy::Secret;
 
 pub struct Application {
     port: u16,
@@ -42,6 +43,7 @@ impl Application {
             connection_pool,
             email_client,
             configuration.application.base_url,
+            configuration.application.hmac_secret,
         )?;
 
         Ok(Self { port, server })
@@ -69,6 +71,7 @@ pub fn run(
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
+    hmac_secret: Secret<String>,
 ) -> Result<Server, std::io::Error> {
     // Wrap the connection in a smart pointer
     let db_pool = web::Data::new(db_pool);
@@ -87,9 +90,13 @@ pub fn run(
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
+            .app_data(web::Data::new(HmacSecret(hmac_secret.clone())))
     })
     .listen(listener)?
     .run();
 
     Ok(server)
 }
+
+#[derive(Clone)]
+pub struct HmacSecret(pub Secret<String>);
